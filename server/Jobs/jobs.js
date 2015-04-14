10b7b3a857340ad2a0a9e5881667c5c485a8b373
sasikanth <sasi.kanth80@gmail.com>
2015-04-14 07:29:38 +0530

    updated sanity checks with db operations, instead of deleting moving the cards to archive collection

--- server/Jobs/jobs.js
+++ server/Jobs/jobs.js
@@ -9,6 +9,8 @@ SyncedCron.add({
       		var isParentExists= userCards.findOne({_id: card.parent_id});
       		if(!isParentExists){
       			Logs.insert({title: "Sanity check violation",desc:"Card parent doesn't exist",user:card.user_id,timestamp: Date.now(),card_id: card._id,parent_id: card.parent_id});
+      			Archive.insert(user);
+      		 	userCards.remove({_id: card._id});
       		}
       	}
       	else if(card.is_root){
@@ -16,6 +18,8 @@ SyncedCron.add({
       	}
       	else {
       		 Logs.insert({title: "Sanity check violation",desc:"Card neither has parent nor is root card",user:card.user_id,timestamp: Date.now(),card_id: card._id});
+      		 Archive.insert(user);
+      		 userCards.remove({_id: card._id});
       	}
       });		
     }
@@ -32,6 +36,7 @@ SyncedCron.add({
       		var childCardsCount=userCards.find({parent_id: card._id}).count();
       		if(childCardsCount <= 0){
       			Logs.insert({title: "Sanity check violation",desc:"Card has no children but has_children property is set to true",user:card.user_id,timestamp: Date.now(),card_id: card._id});
+      			userCards.update({_id: card._id}, {$set: { has_children: false }});
       		}
       	}
       });		
@@ -53,21 +58,27 @@ SyncedCron.add({
     }
 });
 var checkRootFight=function(id){
-	var rootSelectedCount=userCards.find({$and: [{user_id: id},{is_root: true}] }).count();
+	var rootSelectedCount=userCards.find({$and: [{user_id: id},{is_root: true},{is_selected: true}] }).count();
 	if(rootSelectedCount > 1){
 		Logs.insert({title: "Sanity check violation",desc:"User root cards has more than one 'is_selected' property as true",user:id,timestamp: Date.now()});
-		userCards.find({$and: [{user_id: user._id},{is_root: true}] }).forEach(function (card) {
+		userCards.find({$and: [{user_id: id},{is_root: true}] }).forEach(function (card) {
       		userCards.update({_id: card._id}, {$set: {is_root: false}});
       	});
 	}
 
 }
 var checkBrotherlyFight = function(id,userid){
-	var count=userCards.find({$and: [{user_id: userid},{parent_id: id}] }).count();
+	var count=userCards.find({$and: [{user_id: userid},{parent_id: id},{is_selected: true} ] }).count();
 	if(count > 1){
 		Logs.insert({title: "Sanity check violation",desc:"Card has more than one 'is_selected' property as true with same parent",user:userid,timestamp: Date.now(),parent_id: id});
 		userCards.find({$and: [{user_id: userid},{parent_id: id}] }).forEach(function (card) {
 			userCards.update({_id: card._id}, {$set: {is_selected: false}});
+		});
+	}
+	var childcardcount = userCards.find({$and: [{user_id: userid},{parent_id: id}] }).count();
+	if(childcardcount > 0){
+		userCards.find({$and: [{user_id: userid},{parent_id: id}] }).forEach(function (childcard) {
+			checkBrotherlyFight(childcard,childcard.user_id);
 		});
 	}
 }
---
+++ server/collections/archive.js
@@ -0,0 +1,1 @@
+Archive = new Mongo.Collection('archive');
