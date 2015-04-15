8540739fbc91c00db44bc04a1fe3449f87be8ac7
sasikanth <sasi.kanth80@gmail.com>
2015-04-15 14:02:43 +0530

    updated sanity checks and tests

--- server/Jobs/job-tests.js
+++ server/Jobs/job-tests.js
@@ -12,19 +12,17 @@ Meteor.startup(function () {
 	//Test records for No delusional parents
 	var card3= userCards.findOne({_id: "test has_children"});
 	if(!card3){
-		userCards.insert({user_id:"fnAyAeyiJRr6wvRo7",_id: "test has_children",title:"test card",has_children: true});
+		userCards.insert({user_id:"fnAyAeyiJRr6wvRo7",parent_id:"is_selected twice",_id: "test has_children",title:"test card",has_children: true});
 	}
 	//Test records for brotherly fight
-	var card4= userCards.findOne({_id: "test record3"});
-	if(!card4){
-		userCards.insert({user_id:"fnAyAeyiJRr6wvRo7",_id: "test record3",title:"test card"});
-	}
+	userCards.remove({_id: "is_selected twice"});
+	userCards.insert({user_id:"fnAyAeyiJRr6wvRo7",_id: "is_selected twice",title:"test card",is_root:true});
 	var card5= userCards.findOne({_id: "test brotherly fight"});
 	if(!card5){
-		userCards.insert({user_id:"fnAyAeyiJRr6wvRo7",_id: "test brotherly fight",parent_id:"test record3", title:"test card",is_selected: true});
+		userCards.insert({user_id:"fnAyAeyiJRr6wvRo7",_id: "test brotherly fight",parent_id:"is_selected twice", title:"test card",is_selected: true});
 	}
 	var card6= userCards.findOne({_id: "test brotherly fight2"});
 	if(!card6){
-		userCards.insert({user_id:"fnAyAeyiJRr6wvRo7",_id: "test brotherly fight2",parent_id:"test record3", title:"test card",is_selected: true});		
+		userCards.insert({user_id:"fnAyAeyiJRr6wvRo7",_id: "test brotherly fight2",parent_id:"is_selected twice", title:"test card",is_selected: true});		
 	}
 });
--- server/Jobs/jobs.js
+++ server/Jobs/jobs.js
@@ -1,4 +1,20 @@
 SyncedCron.add({
+    name: 'No brotherly fight',
+    schedule: function(parser) {
+      return parser.text('every 1 hour');
+    }, 
+    job: function() {
+        Logs.insert({title: "Sanity check violation",desc:"No brotherly fight job started.",timestamp: Date.now()});
+        Meteor.users.find().forEach(function (user) {
+          checkRootFight(user._id);
+          userCards.find({$and: [{user_id: user._id},{is_root: true}] }).forEach(function (card) {
+            checkBrotherlyFight(card._id,card.user_id);
+          });
+        });
+        Logs.insert({title: "Sanity check violation",desc:"No brotherly fight job ended.",timestamp: Date.now()});
+    }
+});
+SyncedCron.add({
     name: 'No delusional parents',
     schedule: function(parser) {
       return parser.text('every 1 hour');
@@ -17,23 +33,6 @@ SyncedCron.add({
       Logs.insert({title: "Sanity check violation",desc:"No delusional parents job ended.",timestamp: Date.now()});
     }
 });
-  
-SyncedCron.add({
-    name: 'No brotherly fight',
-    schedule: function(parser) {
-      return parser.text('every 1 hour');
-    }, 
-    job: function() {
-        Logs.insert({title: "Sanity check violation",desc:"No brotherly fight job started.",timestamp: Date.now()});
-      	Meteor.users.find().forEach(function (user) {
-      		checkRootFight(user._id);
-      		userCards.find({$and: [{user_id: user._id},{is_root: true}] }).forEach(function (card) {
-      			checkBrotherlyFight(card._id,card.user_id);
-      		});
-      	});
-        Logs.insert({title: "Sanity check violation",desc:"No brotherly fight job ended.",timestamp: Date.now()});
-    }
-});
 SyncedCron.add({
     name: 'No orphan cards',
     schedule: function(parser) {
@@ -44,7 +43,7 @@ SyncedCron.add({
       userCards.find().forEach(function (card) {
         var isParentExists= userCards.findOne({_id: card.parent_id});
       	if(!isParentExists && !card.is_root){
-            Logs.insert({title: "Sanity check violation",desc:"Card doesn't have parent id and the card is not root either",user:card.user_id,timestamp: Date.now(),card_id: card._id});
+            Logs.insert({title: "Sanity check violation",desc:"Card parent id doesn't exists and the card is not a root",user:card.user_id,timestamp: Date.now(),card_id: card._id});
            var ssid={card_id:card._id};
            var cardid=card._id;
            card = _.omit(card, "_id");
@@ -62,25 +61,23 @@ var checkRootFight=function(id){
 	if(rootSelectedCount > 1){
 		Logs.insert({title: "Sanity check violation",desc:"User root cards has more than one 'is_selected' property as true",user:id,timestamp: Date.now()});
 		userCards.find({$and: [{user_id: id},{is_root: true}] }).forEach(function (card) {
-      		userCards.update({_id: card._id}, {$set: {is_root: false}});
-      	});
+      		userCards.update({_id: card._id}, {$set: {is_selected: false}});
+    });
 	}
 
 }
 var checkBrotherlyFight = function(id,userid){
 	var count=userCards.find({$and: [{user_id: userid},{parent_id: id},{is_selected: true} ] }).count();
 	if(count > 1){
-		Logs.insert({title: "Sanity check violation",desc:"Card has more than one 'is_selected' property as true with same parent",user:userid,timestamp: Date.now(),parent_id: id});
+		Logs.insert({title: "Sanity check violation",desc:"Card has more than one 'is_selected' property as true with same parent",user:userid,timestamp: Date.now(),card_id: id});
 		userCards.find({$and: [{user_id: userid},{parent_id: id}] }).forEach(function (card) {
 			userCards.update({_id: card._id}, {$set: {is_selected: false}});
 		});
 	}
-	var childcardcount = userCards.find({$and: [{user_id: userid},{parent_id: id}] }).count();
-	if(childcardcount > 0){
-		userCards.find({$and: [{user_id: userid},{parent_id: id}] }).forEach(function (childcard) {
-			checkBrotherlyFight(childcard,childcard.user_id);
-		});
-	}
+  userCards.find({$and: [{user_id: userid},{parent_id: id}] }).forEach(function (childcard) {
+      checkBrotherlyFight(childcard._id,childcard.user_id);
+  });
+	
 }
 
 Meteor.startup(function () {
