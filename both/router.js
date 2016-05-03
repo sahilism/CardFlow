21118c3eb3f3b55743013bc87191685dbfa0057a
Sasikanth <dronavalli.sasi@gmail.com>
2016-05-04 00:30:40 +0530

    updates

--- both/router.js
+++ both/router.js
@@ -115,10 +115,10 @@ Router.route('/webhook/:id/:cardId', { where: 'server' })
       query  = this.request.query,
       data = this.request.body
       fields = {};
-  		// console.log(user, card, data);
+  		console.log(user, card, data);
      if(user && card && data){
      	userCards.update({ $and: [ { user_id:user }, {parent_id: card}, {is_selected: true} ] }, { $set: { is_selected: false } });
-     	userCards.insert({user_id:user,cardTitle: data.title,has_children: false,is_selected:true,parent_id:card,createdAt:Date.now()});
+     	userCards.insert({user_id:user,cardTitle: data.text,has_children: false,is_selected:true,parent_id:card,createdAt:Date.now()});
     	this.response.statusCode = 200;
 	  	this.response.end( "Valid details" ); 	
      }else{
