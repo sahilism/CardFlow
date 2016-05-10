046b56a7e032ce91a4c3b17a2c1ce8cd30d4a37f
Sasikanth <dronavalli.sasi@gmail.com>
2016-05-10 11:37:17 +0530

    json updates

--- server/methods/xml.js
+++ server/methods/xml.js
@@ -1,43 +1,9 @@
 Meteor.methods({
-  getJson: function (userId) {
+  getProfileJson: function (userId) {
     return getUserJSON(userId);
   }
 });
-var js2xmlparser = Meteor.npmRequire( 'js2xmlparser' );
 
-convertJsonToXml = function( root, data ){
-  return js2xmlparser( root, data );
-};
-// {
-//   "card": [
-//     { "title": "Letters to a Young Poet", "author": "Rainer Maria Rilke" },
-//     { "title": "Changing My Mind", "author": "Zadie Smith" },
-//     { "title": "Managing Oneself", "author": "Peter Drucker" }
-//   ]
-// }
-getUserXml = function(userId){
-  var obj = {};
-  var cardsArr = [];
-  var allCards = getParentXML(userId, 'root')
-  cardsArr =allCards;
-  obj['card'] = allCards;
-  console.log(JSON.stringify(obj));
-  return obj;
-}
-var resCards = []
-getParentXML = function(userId, parent_id){
-  var allCards = userCards.find({ $and: [ { user_id: userId }, { parent_id: parent_id } ] }).fetch();
-  if(allCards.length > 0){
-    allCards.forEach(function (cardInfo) {
-      cardInfo.child_cards = getParentXML(userId, cardInfo._id);
-      resCards.push(cardInfo)
-      return getParentXML(userId, cardInfo._id)
-    });  
-  }else{
-    return userCards.find({ $and: [ { user_id: userId }, { _id: parent_id } ] }).fetch();;
-  }
-  
-}
 
 getUserJSON  = function(userId){
   var userJSON = [];
@@ -71,6 +37,8 @@ getUserJSON  = function(userId){
     delete item.is_completed;
     delete item.notes;
 
+    // item.content = item.cardTitle;
+    // delete item.cardTitle;
     // has parent?
     if(parent) {
       // add item as a child
@@ -80,18 +48,6 @@ getUserJSON  = function(userId){
     // This part determines if the item is a root item or not
     return !parent;
   });
-
+  return result;
   return JSON.stringify(result, 0, 2)
-}
-
-getJSONCards = function(userId, parent_id, userData){
-  var allCards = userCards.find({ $and: [ { user_id: userId }, { parent_id: parent_id } ] }).fetch();
-  if(allCards.length > 0){
-    allCards.forEach(function (cardInfo) {
-      var isExist = $.grep(userData, function(e){ return e.content === parent_id; });
-      if(isExist){
-        
-      }
-    });
-  }
 }
